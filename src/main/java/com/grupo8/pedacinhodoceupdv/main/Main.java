76fcbfe4769a457623740ecf135361da619bdc99
estevam5s <estevamsouzalaureth@gamil.com>
2023-05-27 11:46:26 -0300

    initial commit

--- src/main/java/com/grupo8/pedacinhodoceupdv/main/Main.java
+++ src/main/java/com/grupo8/pedacinhodoceupdv/main/Main.java
@@ -1,9 +1,24 @@
 package com.grupo8.pedacinhodoceupdv.main;
 
-import java.awt.Font;
-import javax.swing.UIManager;
 import com.grupo8.pedacinhodoceupdv.fonts.FontManager;
 import com.grupo8.pedacinhodoceupdv.view.MenuView;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -22,8 +37,15 @@ public class Main extends JFrame {
         setLocationRelativeTo(null);
         setLayout(new BorderLayout());
 
+        // Definir o tema Nimbus para uma aparência mais moderna
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(3, 2, 10, 10));
+        panel.setLayout(new GridLayout(4, 2, 10, 10));
 
         JLabel labelUsername = new JLabel("Username:");
         textFieldUsername = new JTextField();
@@ -32,6 +54,7 @@ public class Main extends JFrame {
         passwordField = new JPasswordField();
 
         buttonLogin = new JButton("Login");
+        JButton buttonRegister = new JButton("Register");
 
         panel.add(labelUsername);
         panel.add(textFieldUsername);
@@ -39,6 +62,8 @@ public class Main extends JFrame {
         panel.add(passwordField);
         panel.add(new JLabel());
         panel.add(buttonLogin);
+        panel.add(new JLabel());
+        panel.add(buttonRegister);
 
         add(panel, BorderLayout.CENTER);
 
@@ -48,42 +73,145 @@ public class Main extends JFrame {
                 String username = textFieldUsername.getText();
                 String password = new String(passwordField.getPassword());
 
-                // Verificar as credenciais do cliente aqui
-                // Você pode adicionar lógica para autenticar o cliente
-                // Se as credenciais forem válidas, exiba o MenuView e feche a tela de login
-
-                if (username.equals("admin") && password.equals("senha")) {
+                // Autenticar com o banco de dados
+                if (authenticateUser(username, password)) {
                     dispose(); // Fecha a tela de login
                     showMenuView(); // Exibe o MenuView
-
-                    // FontManager fontManager = new FontManager();
-                    // Font futuraPT16 = fontManager.carregarFont("/fontes/FuturaPT.otf",
-                    // Font.PLAIN, 16);
-                    // Font futuraPT14 = fontManager.carregarFont("/fontes/FuturaPT.otf",
-                    // Font.PLAIN, 14);
-                    // Font futuraPT14Bold = fontManager.carregarFont("/fontes/FuturaPT.otf",
-                    // Font.BOLD, 14);
-                    // Font futuraPT13 = fontManager.carregarFont("/fontes/FuturaPT.otf",
-                    // Font.PLAIN, 13);
-                    // UIManager.put("dateChooserCombo", futuraPT13);
-                    // UIManager.put("Button.font", futuraPT16);
-                    // UIManager.put("RadioButton.font", LoginViewturaPT13);
-                    // UIManager.put("Label.font", futuraPT14);
-                    // UIManager.put("Panel.font", futuraPT16);
-                    // UIManager.put("Table.font", futuraPT13);
-                    // UIManager.put("TableHeader.font", futuraPT14Bold);
-                    // UIManager.put("TextField.font", futuraPT13);
-                    // UIManager.put("FormattedTextField.font", futuraPT13);
-                    // UIManager.put("TextArea.font", futuraPT13);
-                    // new MenuView().setVisible(true);
                 } else {
                     JOptionPane.showMessageDialog(Main.this, "Credenciais inválidas", "Erro de Login",
                             JOptionPane.ERROR_MESSAGE);
                 }
             }
         });
+
+        buttonRegister.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose(); // Fecha a tela de login
+                showRegisterView(); // Exibe a tela de registro
+            }
+        });
     }
 
+    private boolean authenticateUser(String username, String password) {
+        // Configurações do banco de dados
+        String url = "jdbc:mysql://localhost:3306/DBPEDACINHODOCEU?useTimezone=true&serverTimezone=UTC";
+        String dbUsername = "user";
+        String dbPassword = "user";
+
+        try (Connection conn = DriverManager.getConnection(url, dbUsername, dbPassword)) {
+            String sql = "SELECT COUNT(*) FROM users WHERE username = ? AND password = ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+            int count = rs.getInt(1);
+            return count > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    public class RegisterView extends JFrame {
+        private final Main parent;
+        private JTextField textFieldUsername;
+        private JPasswordField passwordField;
+        private JButton buttonRegister;
+        private JButton buttonBack;
+
+        public RegisterView(final Main parent) {
+            this.parent = parent;
+            setTitle("Register");
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            setSize(300, 200);
+            setLocationRelativeTo(null);
+            setLayout(new BorderLayout());
+
+            JPanel panel = new JPanel();
+            panel.setLayout(new GridLayout(4, 2, 10, 10));
+
+            JLabel labelUsername = new JLabel("Username:");
+            textFieldUsername = new JTextField();
+
+            JLabel labelPassword = new JLabel("Password:");
+            passwordField = new JPasswordField();
+
+            buttonRegister = new JButton("Register");
+            buttonBack = new JButton("Back");
+
+            panel.add(labelUsername);
+            panel.add(textFieldUsername);
+            panel.add(labelPassword);
+            panel.add(passwordField);
+            panel.add(new JLabel());
+            panel.add(buttonRegister);
+            panel.add(new JLabel());
+            panel.add(buttonBack);
+
+            add(panel, BorderLayout.CENTER);
+
+            buttonRegister.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String username = textFieldUsername.getText();
+                    String password = new String(passwordField.getPassword());
+
+                    registerUser(username, password);
+                }
+            });
+
+            buttonBack.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    dispose(); // Fecha a tela de registro
+                    parent.showMenuView(); // Exibe a tela de login novamente
+                }
+            });
+        }
+
+        private void registerUser(String username, String password) {
+            String url = "jdbc:mysql://localhost:3306/DBPEDACINHODOCEU?useTimezone=true&serverTimezone=UTC";
+            String dbUsername = "user";
+            String dbPassword = "user";
+
+            try {
+                Connection connection = DriverManager.getConnection(url, dbUsername, dbPassword);
+
+                String sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+                PreparedStatement statement = connection.prepareStatement(sql);
+                statement.setString(1, username);
+                statement.setString(2, password);
+
+                int rowsInserted = statement.executeUpdate();
+                if (rowsInserted > 0) {
+                    JOptionPane.showMessageDialog(this, "Registration successful", "Success",
+                            JOptionPane.INFORMATION_MESSAGE);
+                    dispose(); // Fecha a tela de registro
+                    parent.showMenuView(); // Exibe a tela de login novamente
+                } else {
+                    JOptionPane.showMessageDialog(this, "Registration failed", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+
+                statement.close();
+                connection.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    private void showRegisterView() {
+        RegisterView registerView = new RegisterView(this);
+        registerView.setVisible(true);
+    }
+
+    protected void registerUser(String username, String password) {
+    }
+
+    @SuppressWarnings("unused")
     private void showMenuView() {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
